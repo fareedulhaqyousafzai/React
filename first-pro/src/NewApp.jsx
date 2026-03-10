05739c25869c99835a11e1097375916ace1e4c1e
Fareed UL Haq Yousafzai <fareedulhaqyousafzai@gmail.com>
2026-03-10 16:36:53 +0500

    useState(counterapp)

--- first-pro/src/NewApp.jsx
+++ first-pro/src/NewApp.jsx
@@ -1,6 +1,25 @@
-function MyApp(){
-    return(
-        <h1>PAKISTAN</h1>
-    )
+import React, { useState } from "react";
+import "./newApp.css";
+
+function MyApp() {
+  const [counterValu, setCounterValu] = useState(0);
+  const incrementValue = () => {
+    setCounterValu(counterValu + 1);
+  };
+  const decrementValue = () => {
+    setCounterValu(counterValu - 1);
+  };
+  return (
+   <div className="counterApp">
+    <div className="counterContainer">
+        <button className="decrementValue" onClick={decrementValue}>-</button>
+        
+        <span className="counterValue" >{counterValu}</span>
+        
+        <button className="incrementValue" onClick={incrementValue}>+</button>
+    </div>
+</div>
+  );
 }
+
 export default MyApp;
